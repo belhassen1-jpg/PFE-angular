29e7516bab3eeb431b8498e0e54210fa8df62f6d
belhassen1-jpg <belhassen.knani@esprit.tn>
2024-06-24 19:53:24 +0100

    update

--- home-angular/src/environments/environment.ts
+++ home-angular/src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  urlBackend:'http://192.168.1.100:8090/GRH/'
+  urlBackend:'https://192.168.1.100:8090/GRH/'
 };
 
 /*
- * Fcsor easier debugging in development mode, you can import the following file
- * to cssbelhaignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
